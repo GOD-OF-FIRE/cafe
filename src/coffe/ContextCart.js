4ce5d6ae654870c4c6f11665fb5235e912c96bbe
Kushagra Gupta <kushagra357@gmail.com>
2022-03-13 16:23:51 +0530

    March 13th Test

---
+++ src/coffe/ContextCart.js
@@ -0,0 +1,77 @@
+import React, { useContext } from "react";
+import Items from "./Items";
+import { CartContext } from "./Cart";
+
+const ContextCart = () => {
+  const { item, clearCart, totalItem, totalAmount } = useContext(CartContext);
+
+  if (item.length === 0) {
+    return (
+      <>
+        <header>
+          <div className="continue-shopping">
+            🍥
+            <h3>CafeBakery Cakes</h3>
+          </div>
+
+          <div className="cart-icon">
+            🧺
+            <p>{totalItem}</p>
+          </div>
+        </header>
+
+        <section className="main-cart-section">
+          <h1>shopping Cart</h1>
+          <p className="total-items">
+            you have <span className="total-items-count">{totalItem} </span>{" "}
+            items in shopping cart
+          </p>
+        </section>
+      </>
+    );
+  }
+
+  return (
+    <>
+      <header>
+        <div className="continue-shopping">
+          🍥
+          <h3>continue shopping</h3>
+        </div>
+
+        <div className="cart-icon">
+          🧺
+          <p>{totalItem}</p>
+        </div>
+      </header>
+
+      <section className="main-cart-section">
+        <h1>shopping Cart</h1>
+        <p className="total-items">
+          you have <span className="total-items-count">{totalItem} </span> items
+          in shopping cart
+        </p>
+
+        <div className="cart-items">
+          <div className="cart-items-container">
+              {item.map((curItem) => {
+                return <Items key={curItem.id} {...curItem} />;
+              })}
+          </div>
+        </div>
+
+        <div className="card-total">
+          <h3>
+            Cart Total : <span>{totalAmount}₹</span>
+          </h3>
+          <button>checkout</button>
+          <button className="clear-cart" onClick={clearCart}>
+            Clear Cart
+          </button>
+        </div>
+      </section>
+    </>
+  );
+};
+
+export default ContextCart;
